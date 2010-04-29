fcb94716653f87491c23fe2e3e29a9205d248fe9
Stefan SIMHANDL <s0725240@ti8.(none)>
2010-04-29 19:10:47 +0200

    linebuffer	... vga fertig implementiert, BKSP und ENTER funktioniert 						... Speicher muß noch eingebunden werden

--- Programm/Calculator/src/line_buffer_arc_beh.vhd
+++ Programm/Calculator/src/line_buffer_arc_beh.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.textmode_vga_pkg.all;
---use work.led_pkg.all;
+use ieee.std_logic_unsigned.all;
 
 
 architecture beh of line_buffer is
@@ -15,39 +15,15 @@ architecture beh of line_buffer is
 	type LINEBUFFER_FSM_STATE_TYPE is
     (CHECK_ASCII, ENTER_1, ENTER_2, BKSP_1, BKSP_2, BKSP_3, SAVE_VALUE, WAIT_STATE);
   signal lb_fsm_state, lb_fsm_state_next, save_next_state, save_next_state_next : LINEBUFFER_FSM_STATE_TYPE;
---	signal ready, ready_next  : std_logic := '0';	
 	signal vga_command_next : std_logic_vector(COMMAND_SIZE - 1 downto 0);
   signal vga_command_data_next : std_logic_vector(3 * COLOR_SIZE + CHAR_SIZE - 1 downto 0);
-	signal col_cnt, col_cnt_next  : integer;
-	signal new_ascii_in_sig, vga_free_sig : std_logic := '0';
+	signal col_cnt, col_cnt_next  : std_logic_vector(7 downto 0);
+	signal vga_free_sig, once, once_next : std_logic := '0';
 
-	function my_rising_edge(value : in std_logic) return std_logic is
-		begin
-			if value = '1' then
-				return '1';
-			else
-				return '0';
-			end if;
-	end function;  
 
 begin
 
-
---	led_inst : led
---    generic map
---    (
---      RESET_VALUE => '0'
---    )
---    port map
---    (
---      sys_clk => sys_clk,
---      sys_res_n => sys_res_n,
---      sense => led_sig,
---    	led_on_off => led_a
---		);
- 
-
-	next_state : process(lb_fsm_state, new_ascii_in_sig, ascii_sign_in, vga_free, save_next_state, col_cnt)
+	next_state : process(lb_fsm_state, new_ascii_in, ascii_sign_in, vga_free, save_next_state, col_cnt)
   begin
     
 		lb_fsm_state_next <= lb_fsm_state;
@@ -56,18 +32,18 @@ begin
     case lb_fsm_state is
 		
 			when CHECK_ASCII =>
-				if new_ascii_in_sig = '1' then
+				if new_ascii_in = '1' then
 					case ascii_sign_in is
 						-- ENTER
 						when x"03" =>
 							lb_fsm_state_next <= ENTER_1;
 						-- BKSP
 						when x"08" =>
-							if col_cnt /= 0 then
+							if col_cnt /= x"00" then
 								lb_fsm_state_next <= BKSP_1;
 							end if;
 						-- other value
-						when others =>
+						when others =>	
 							lb_fsm_state_next <= SAVE_VALUE;	
 					end case;
 				end if;
@@ -93,20 +69,13 @@ begin
 				end if;
 			when BKSP_3 =>  
 				if vga_free = '0' then
-					lb_fsm_state_next <= CHECK_ASCII; 
+					lb_fsm_state_next <= CHECK_ASCII;	
 				end if;		
 			when SAVE_VALUE => 
---				if vga_free = '1' then
-					lb_fsm_state_next <= CHECK_ASCII; 
---				end if;	
---			when WAIT_PREE_STATE =>
---				if vga_free = '0' then
---					lb_fsm_state_next <= WAIT_STATE;
---				else
---					lb_fsm_state_next <= save_next_state;
---				end if;
+				if vga_free = '0' then
+					lb_fsm_state_next <= CHECK_ASCII;	
+				end if;	
 			when WAIT_STATE =>
---					led_sig <= '1';
 				if vga_free = '1' then
 					lb_fsm_state_next <= save_next_state;
 				end if;
@@ -114,12 +83,14 @@ begin
   end process next_state;
 
 
-	output : process(lb_fsm_state, col_cnt, ascii_sign_in, vga_free)
+	output : process(lb_fsm_state, col_cnt, ascii_sign_in, vga_free, once)
+	
 	begin
 	--	start_calc <= '0';
 		vga_command_next <= COMMAND_NOP;
 		vga_command_data_next <= DEFAULT_VGA_DATA;
 		col_cnt_next <= col_cnt;
+		once_next <= once;
 
 		case lb_fsm_state is
 			when ENTER_1 =>
@@ -131,13 +102,13 @@ begin
 				if vga_free = '1' then
 					vga_command_next <= COMMAND_SET_CHAR;
 					vga_command_data_next(7 downto 0) <= x"0A"; 
-					col_cnt_next <= 0; 
+					col_cnt_next <= x"00"; 
 --				start_calc <= '1';
 				end if;
 			when BKSP_1 =>
 				if vga_free = '1' then
 					vga_command_next <= COMMAND_SET_CURSOR_COLUMN;
-					vga_command_data_next(7 downto 0) <= std_logic_vector(to_unsigned((col_cnt - 1), 8));		
+					vga_command_data_next(7 downto 0) <= (col_cnt - '1');
 				end if;
 			when BKSP_2 =>
 				if vga_free = '1' then
@@ -147,16 +118,24 @@ begin
 			when BKSP_3 =>
 				if vga_free = '1' then
 					vga_command_next <= COMMAND_SET_CURSOR_COLUMN;
-					vga_command_data_next(7 downto 0) <= std_logic_vector(to_unsigned(col_cnt, 8));	
-					col_cnt_next <= (col_cnt - 1);
+					vga_command_data_next(7 downto 0) <= (col_cnt - '1');	
+					if once = '0' then	
+						col_cnt_next <= (col_cnt - '1');
+					end if;
+					once_next <= '1';
 				end if;
 			when SAVE_VALUE =>
 				if vga_free = '1' then
-					col_cnt_next <= (col_cnt + 1);
 					vga_command_next <= COMMAND_SET_CHAR;
 					vga_command_data_next(31 downto 8) <= x"FFFFFF";
 					vga_command_data_next(7 downto 0) <= ascii_sign_in;
+					if once = '0' then	
+						col_cnt_next <= (col_cnt + '1');
+					end if;
+					once_next <= '1';
 				end if;
+			when CHECK_ASCII =>
+				once_next <= '0';	
 			when others =>
 				null;
 		end case;
@@ -177,18 +156,16 @@ begin
   begin
     if sys_res_n = '0' then
       lb_fsm_state <= CHECK_ASCII;
-			col_cnt <= 0;
+			col_cnt <= (others => '0');	-- x"00"
     elsif rising_edge(sys_clk) then
---			vga_free_sig <= my_rising_edge(vga_free);
-			new_ascii_in_sig <= my_rising_edge(new_ascii_in);
 			lb_fsm_state <= lb_fsm_state_next;
 			col_cnt <= col_cnt_next;
 			save_next_state <= save_next_state_next;
 			vga_command <= vga_command_next;
 			vga_command_data <= vga_command_data_next;
+			once <= once_next;
 		end if;
 	end process sync;
-
 
 end architecture beh;
 
