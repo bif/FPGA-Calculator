f10105c1fedfdef361de5b6a65ddb2ab27959662
Stefan SIMHANDL <s0725240@ti6.(none)>
2010-05-03 10:23:03 +0200

    synchroner Dualport RAM

---
+++ Programm/Calculator/src/dp_ram_ent.vhd
@@ -0,0 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity dp_ram is
+  generic
+  (
+		ADDR_WIDTH : integer range 1 to integer‘high;
+		DATA_WIDTH : integer range 1 to integer‘high
+  );
+  port
+  (
+		clk       : in std_logic;
+		address1 : in std_logic_vector(ADDR_WIDTH -   1 downto 0);
+		data_out1 : out std_logic_vector(DATA_WIDTH - 1 downto 0);
+		wr1       : in std_logic;
+		data_in1 : in std_logic_vector(DATA_WIDTH -   1 downto 0);
+		address2 : in std_logic_vector(ADDR_WIDTH -   1 downto 0);
+		data_out2 : out std_logic_vector(DATA_WIDTH - 1 downto 0)
+	);
+end entity dp_ram;
+
+
