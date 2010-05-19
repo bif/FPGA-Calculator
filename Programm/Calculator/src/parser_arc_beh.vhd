3707147c97180c3dc766dc508e2eadded7ddefd8
Stefan SIMHANDL <s0725240@ti5.(none)>
2010-05-19 18:34:12 +0200

    TODO: Parser Simulation - Funktionalitaet testen ... Testbench/ DoFile

--- Programm/Calculator/src/parser_arc_beh.vhd
+++ Programm/Calculator/src/parser_arc_beh.vhd
@@ -12,7 +12,7 @@ architecture beh of parser is
 	signal convert_count, convert_count_next, line_count, line_count_next, start_pos, start_pos_next, end_pos, end_pos_next : std_logic_vector(ADDR_WIDTH - 1 downto 0);
 	signal data : std_logic_vector(DATA_WIDTH - 1 downto 0);
 	signal operator_next : std_logic_vector(1 downto 0);
-	signal operand_next : std_logic_vector(31 downto 0);
+	signal operand_next, last_operand : std_logic_vector(31 downto 0);
 	signal once, once_next, space, space_next, leading_sign, leading_sign_next, end_of_op_next, parse_ready_next, state_ready, state_ready_next : std_logic;
 
 begin
@@ -51,7 +51,9 @@ begin
 
 
 
-	output : process(parser_fsm_state, data, once, space, line_count, leading_sign, state_ready, start_pos, error_sig, convert_count)
+	output : process(parser_fsm_state, data, once, space, line_count, leading_sign, state_ready, start_pos, error_sig, convert_count, last_operand)
+
+--	variable i : unsigned range 1000000000 to 0 := 0;
 
   begin
 		leading_sign_next <= leading_sign;
@@ -65,6 +67,7 @@ begin
 		operator_next <= "00";
 		end_of_op_next <= '0';
 		convert_count_next <= convert_count;
+		operand_next <= last_operand;
 
 		case parser_fsm_state is
 			when READY =>
@@ -198,8 +201,75 @@ begin
 				end if;
 				-- Start of Operand = start_pos, End of Operand = end_pos
 --TODO
-					
+--					i := (10 * unsigned(convert_count));
+					convert_count_next <= std_logic_vector(unsigned(convert_count) - 1);
+					start_pos_next <= std_logic_vector(unsigned(start_pos) + 1);
+					if unsigned(convert_count) >= 10 then
+									case data is							
+										when x"31" =>
+											operand_next <= std_logic_vector(unsigned(last_operand) + unsigned(convert_count));
 
+										when x"32" =>
+											operand_next <= std_logic_vector(unsigned(last_operand) + (unsigned(convert_count) * 20));
+
+										when x"33" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + (unsigned(convert_count) * 30));
+
+										when x"34" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + (unsigned(convert_count) * 40));
+
+										when x"35" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + (unsigned(convert_count) * 50));
+
+										when x"36" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + (unsigned(convert_count) * 60));
+
+										when x"37" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + (unsigned(convert_count) * 70));
+
+										when x"38" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + (unsigned(convert_count) * 80));
+
+										when x"39" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + (unsigned(convert_count) * 90));
+
+										when others =>
+											null;
+									end case;
+						elsif unsigned(convert_count) = 0 then
+									case data is							
+										when x"31" =>
+											operand_next <= std_logic_vector(unsigned(last_operand) + 1);
+
+										when x"32" =>
+											operand_next <= std_logic_vector(unsigned(last_operand) + 2);
+
+										when x"33" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + 3);
+
+										when x"34" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + 4);
+
+										when x"35" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + 5);
+
+										when x"36" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + 6);
+
+										when x"37" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + 7);
+
+										when x"38" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + 8);
+
+										when x"39" => 
+											operand_next <= std_logic_vector(unsigned(last_operand) + 9);
+
+										when others =>
+											null;
+									end case;
+						state_ready_next <= '1';
+					end if;
 			when ERROR_STATE =>
 			--TODO:						
 				null;
@@ -212,12 +282,13 @@ begin
   assert RESET_VALUE = '0' or RESET_VALUE = '1' report
     "RESET_VALUE may only be 0 or 1!" severity failure;
 
-  sync : process(sys_clk, sys_res_n)
+  sync : process(sys_clk, sys_res_n, parser_fsm_state)
   begin
     if sys_res_n = '0' then
       parser_fsm_state <= READY;
 			line_count <= (others => '0');
 			space <= '0';
+			last_operand <= x"00000000";
 		elsif (sys_clk'event and sys_clk = '1') then
 			space <= space_next;
 			parser_fsm_state <= parser_fsm_state_next;
@@ -225,11 +296,17 @@ begin
 			once <= once_next;
 			data <= data_in;
 			line_count <= line_count_next;
-			addr_lb <= line_count;
+			case parser_fsm_state is
+				when CONVERT_TO_INT =>
+					addr_lb <= start_pos;
+				when others =>
+					addr_lb <= line_count;
+			end case;
 			end_of_operation <= end_of_op_next;
 			parse_ready <= parse_ready_next;
 			operator <= operator_next;
 			operand <= operand_next;	
+			last_operand <= operand_next;
 			state_ready <= state_ready_next;
 			leading_sign <= leading_sign_next;
 			start_pos <= start_pos_next;
